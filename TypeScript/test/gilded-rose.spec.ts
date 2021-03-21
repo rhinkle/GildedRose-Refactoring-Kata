4199fd458250ef5fc61b8d6dd311dc6849ceaa26
Ryan Hinkle <ryan.hinkle@integral.io>
2021-03-21 14:02:19 -0400

    create testing cases around non special cases with +5 Dex vest.

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -9,4 +9,28 @@ describe('Gilded Rose', function () {
         expect(items.length).equal(0);
     });
 
+
+    describe('Non-special cases with +5 Dex Vest', () => {
+        let rose: GildedRose;
+        let items: Array<Item>;
+        const stock = [
+            new Item("+5 Dexterity Vest", 10, 20),
+            new Item("+5 Dexterity Vest", 10, 0),
+            new Item("+5 Dexterity Vest", 10, 50),
+        ];
+
+        beforeEach(() => {
+            rose = new GildedRose(stock);
+            items = rose.updateQuality();
+        });
+
+        it('at the end of the day lower by 1 Quality and SellIn', () => {
+            expect(items[0].quality).equal(19);
+            expect(items[0].sellIn).equal(9);
+        });
+
+        it('quality of an item is never negative', () => {
+            expect(items[1].quality).equal(0);
+        });
+    });
 });
