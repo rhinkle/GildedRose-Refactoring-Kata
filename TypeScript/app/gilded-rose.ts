e7f7a0820f879c277769c2cce54cd4ba14bdbb9d
Ryan Hinkle <ryan.hinkle@integral.io>
2021-03-21 13:27:35 -0400

    Revert to original starting spot

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,4 @@
+
 export class Item {
     name: string;
     sellIn: number;
@@ -13,37 +14,38 @@ export class Item {
 export class GildedRose {
     items: Array<Item>;
 
-    private legendary_items = [
-        'Sulfuras, Hand of Ragnaros'
-    ];
-
     constructor(items = [] as Array<Item>) {
         this.items = items;
     }
 
-
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                    this.items[i].quality--
+                if (this.items[i].quality > 0) {
+                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                        this.items[i].quality = this.items[i].quality - 1
+                    }
                 }
             } else {
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality++;
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].quality < 50) {
+                    this.items[i].quality = this.items[i].quality + 1
+                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 11) {
-                            this.items[i].quality++;
+                            if (this.items[i].quality < 50) {
+                                this.items[i].quality = this.items[i].quality + 1
+                            }
                         }
                         if (this.items[i].sellIn < 6) {
-                            this.items[i].quality++;
+                            if (this.items[i].quality < 50) {
+                                this.items[i].quality = this.items[i].quality + 1
+                            }
                         }
                     }
                 }
             }
-
-            this.ageItem(i);
-
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].sellIn = this.items[i].sellIn - 1;
+            }
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
@@ -64,11 +66,5 @@ export class GildedRose {
         }
 
         return this.items;
-    }
-
-    private ageItem(index) {
-        if (this.legendary_items.indexOf(this.items[index].name)) {
-            this.items[index].sellIn--;
-        }
     }
 }
